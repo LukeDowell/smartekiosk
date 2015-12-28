e5d7154245e578956af557d52b509d6cfadb523b
Luke <lukedowell@gmail.com>
2015-12-28 01:14:10 -0600

    Adds javafx

---
+++ src/kiosk/src/com/nerdery/smartecarte/gui/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.nerdery.smartecarte.gui;
+
+import com.nerdery.smartecarte.KioskApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ * Created by Luke on 12/28/2015.
+ */
+public class WelcomeController {
+
+    @FXML
+    GridPane gridPane;
+
+    @FXML
+    public void btnPressed(ActionEvent event) {
+        Stage stage = (Stage) gridPane.getScene().getWindow();
+        stage.setScene(KioskApplication.cartSelectionScene);
+    }
+}
